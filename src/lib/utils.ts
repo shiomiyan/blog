8ef5fea7e8c3b81d940374d0ec423788b0828072
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2024-11-25 20:40:18 +0900

    remove unused compoment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,6 @@
 import { EXTERNAL_FEEDS } from "@consts";
 import { getCollection, type CollectionEntry } from "astro:content";
-import { clsx, type ClassValue } from "clsx";
 import Parser from "rss-parser";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
 
 export function formatDate(date: Date) {
 	return Intl.DateTimeFormat("en-US", {
