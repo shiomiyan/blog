787f98d4a0390c985082b150007894a0f75d9377
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2024-12-09 12:35:58 +0900

    Add helper script

---
+++ bin/create-post.ts
@@ -0,0 +1,46 @@
+/**
+ * Helper script for create post directory and mdx file with following format.
+ *
+ * ```
+ * src/content/posts/<8-digit-datetime>-<slug>/index.mdx
+ * ```
+ *
+ * Run from npm script.
+ *
+ * ```
+ * npm run create-post -- '<TITLE>' '<SLUG>'
+ * ```
+ *
+ */
+
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const nowISO = new Date().toISOString();
+const suffix = nowISO.split("T")[0].replace(/-/g, "");
+
+const title = process.argv[2];
+const slug = process.argv[3];
+if (slug.match(/[^a-z0-9\-]/g)) {
+	console.error(`Do not use not allowed characters`);
+	process.exit(1);
+}
+
+const target = `src/content/posts/${suffix}-${slug}/index.mdx`;
+const content = `---
+title: ${title}
+date: ${nowISO}
+description: ""
+tags:
+  - random
+---
+`;
+
+fs.mkdir(path.dirname(target), { recursive: true }, (err) => {
+	if (err) console.error(err);
+	fs.writeFile(target, content, (err) => {
+		if (err) console.error(err);
+	});
+});
+
+console.log(target);
