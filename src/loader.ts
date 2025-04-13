b742abddc243d5c4cae61c91fb0f77312ba2cbea
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2025-04-13 21:23:10 +0900

    small fix loader

--- src/loader.ts
+++ src/loader.ts
@@ -5,25 +5,36 @@ import Parser from "rss-parser";
 export const rssLoader = (options: { url: string; tag: string }): Loader => {
 	return {
 		name: "rss-loader",
-		load: async ({ store, logger, parseData }): Promise<void> => {
+		load: async ({
+			store,
+			logger,
+			parseData,
+			generateDigest,
+		}): Promise<void> => {
 			logger.info(`Loading RSS feed from ${options.url}`);
+
 			type itemDef = { title: string; link: string; pubDate: Date };
 			const parser = new Parser<{ item: itemDef[] }>();
 			const feed = await parser.parseURL(options.url);
+
+			store.clear();
+
 			for (const item of feed.items) {
 				const itemId = item.guid || item.id;
-				if (!itemId) continue;
+				if (!itemId || !item.pubDate) continue;
 
 				const data = await parseData({
 					id: itemId,
 					data: {
 						title: item.title,
 						link: item.link,
-						date: new Date(item.pubDate || ""),
+						date: new Date(item.pubDate),
 						tags: [options.tag],
 					},
 				});
-				store.set({ id: itemId, data });
+				const digest = generateDigest(data);
+
+				store.set({ id: itemId, data, digest });
 			}
 		},
 		schema: rssSchema,
