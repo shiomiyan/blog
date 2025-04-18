ec688844fd20859f63f3b69394e2f4174293948e
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2025-04-18 14:29:12 +0900

    use marked

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,34 +1,37 @@
 import rss from "@astrojs/rss";
 import { SITE } from "@consts";
 import { getCollection } from "astro:content";
-import MarkdownIt from "markdown-it";
+import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
 
 type Context = {
 	site: string;
 };
 
-const parser = new MarkdownIt();
-
 export async function GET(context: Context) {
 	const posts = (await getCollection("posts")).filter(
 		(post) => !post.data.draft,
 	);
 
-	const items = [...posts].sort(
-		(a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
+	const items = await Promise.all(
+		posts.map(async (item) => {
+			const { title, description, date } = item.data;
+			const content = sanitizeHtml(await marked.parse(item.body));
+			return {
+				title,
+				description,
+				pubDate: date,
+				link: `/${item.collection}/${item.slug}/`,
+				content,
+			};
+		}),
 	);
+	items.sort((a, b) => b.pubDate.getTime() - a.pubDate.getTime());
 
 	return rss({
 		title: SITE.TITLE,
 		description: SITE.DESCRIPTION,
 		site: context.site,
-		items: items.map((item) => ({
-			title: item.data.title,
-			description: item.data.description,
-			pubDate: item.data.date,
-			link: `/${item.collection}/${item.slug}/`,
-			content: sanitizeHtml(parser.render(item.body)),
-		})),
+		items,
 	});
 }
