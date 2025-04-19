1cc7d33ed73d7cb80e11cc5146e54773f37377bb
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2025-04-19 20:51:03 +0900

    consts -> constants

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,5 @@
 import rss from "@astrojs/rss";
-import { SITE } from "@consts";
+import { SITE } from "@constants";
 import { getCollection } from "astro:content";
 import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
