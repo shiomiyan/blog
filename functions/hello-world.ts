cb0b6736dcb25f3fbff070829e7387c498f02dfa
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2025-04-22 20:27:36 +0900

    refactor

--- functions/hello-world.ts
+++
@@ -1,9 +0,0 @@
-export const onRequestGet: PagesFunction<Env> = async (context) => {
-	const teapot = "🫖";
-	return new Response(JSON.stringify({ whoami: teapot }), {
-		status: 418,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-};
