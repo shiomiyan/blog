91d6e1fc1de4ceb498bc30d357cbb22a4a8d19f4
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2025-05-13 20:19:35 +0900

    black

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,13 @@ export default {
         sans: ['ui-sans-serif,sans-serif,system-ui,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji"'],
         mono: ['ui-monospace,SFMono-Regular,Consolas,Menlo,Monaco,Liberation Mono,Courier New,monospace'],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "#000",
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
